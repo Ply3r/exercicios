b795fdcec7fd744c2071be81158957c3c5d668cc
Leandro Henrique <leandro.resende@vollsolutions.com.br>
2023-04-13 19:23:24 -0300

    fix quick sort

--- ruby/quick_sort.rb
+++ ruby/quick_sort.rb
@@ -5,31 +5,15 @@ require 'byebug'
 def quick_sort(array)
   return array if array.length <= 1
 
-  sum_all = array.reduce(:+)
-  pivot = sum_all / array.length
-  left, right = array.partition { |num| num <= pivot }
-
-  if left.empty? || right.empty?
-    middle = (array.length / 2).floor - 1
-    left, right = array[0..middle], array[middle + 1..array.length - 1]
+  left, right = [], []
+  pivot = array[0]
+  for i in 1..array.length - 1
+    left << array[i] if array[i] < pivot
+    right << array[i] if array[i] >= pivot
   end
 
-  left = quick_sort(left)
-  right = quick_sort(right)
-
-  intercalition_order(left, right)
+  quick_sort(left) + [pivot] + quick_sort(right)
 end
 
-def intercalition_order(left, right)
-  result = []
-
-  while result.length < left.length + right.length
-    break if left.empty? || right.empty?
-    result << (left.first < right.first ? left.shift : right.shift)
-  end
-
-  result.concat(left).concat(right)
-end
-
-array = randint_array(1000000)
+array = randint_array(10)
 sorted_array = quick_sort(array)
